7c3f674ffc1103f34e526fd02e861a379017473c
jiupai <zhuangmin@gmail.com>
2011-07-03 18:48:44 +0800

    forgot again

--- db/migrate/20110703101640_load_data.rb
+++ db/migrate/20110703101640_load_data.rb
@@ -4,13 +4,14 @@ class LoadData < ActiveRecord::Migration
     cateNew = Category.find_by_name("running")
     
     if metadataNew == nil
-      metadataNew = Metadata.new()
+      metadataNew = Metadata.new
       metadataNew.name = "hour"
       metadataNew.data_type = 1
       metadataNew.save
     end
       
     if cateNew == nil
+      cateNew = Category.new
       cateNew.name = "running"
       cateNew.save
     end
