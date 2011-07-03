b399ec1d43a9819a2b5fd4749a5684f27ff753c8
jiupai <zhuangmin@gmail.com>
2011-07-03 18:45:41 +0800

    shooott

--- db/migrate/20110703101640_load_data.rb
+++ db/migrate/20110703101640_load_data.rb
@@ -1,17 +1,20 @@
 class LoadData < ActiveRecord::Migration
   def self.up
     metadataNew = Metadata.find_by_name("hour")
-    cateNew = Category.find_by_nam("running")
+    cateNew = Category.find_by_name("running")
     
     if metadataNew == nil
       metadataNew = Metadata.new()
       metadataNew.name = "hour"
       metadataNew.data_type = 1
       metadataNew.save
+    end
       
     if cateNew == nil
       cateNew.name = "running"
       cateNew.save
+    end
+    
   end
 
   def self.down
